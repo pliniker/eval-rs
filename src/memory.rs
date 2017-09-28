ba6ed44e243320b955f0c68ff475683ea142af55
Peter Liniker <peter.liniker+github@gmail.com>
2017-09-27 21:49:42 -0400

    Mem trait design ideas

--- src/memory.rs
+++ src/memory.rs
@@ -9,40 +9,64 @@ use std::ptr;
 use memalloc::{allocate, deallocate};
 
 
-// we need two allocators:
-// 1. movable objects
-// 2. immovable objects
-//
-// Either:
-// trait Allocator { fn alloc(); fn alloc_immovable(); }
-// heap: Heap;
-//
-// or
-// trait Allocator { fn alloc(); }
-// heap: MovableHeap; heap: StaticHeap;
-// (two parameters everywhere)
+/*
+What is it I want?
+
+ * A vm has a stack, code and a pc
+ * A stack has a heap
+ * A heap has:
+ * an allocator
+ * a GC
+
+A symbol table has a hashmap and an append-only-heap
+
+1. the stack is the window into the heap
+2. an stack context allocates new heap pointers but stores them straight onto the stack
+
+1. a mutator is subservient to the memory structure (stack + heap)
+2. to describe heap operations in terms of the heap *owning* it's memory and allocator
+3. heap operations can't escape a heap borrow context
+
+heap.mutate(|mem| ptr = mem.alloc())
+ */
+
 
 /// An allocator trait that is expected throughout the source code. This should
 /// serve to abstract any allocator backing, allowing easier experimentation.
 pub trait Allocator {
+    /// Allocate and move an object into the space. This object may be garbage
+    /// collected or moved.
     fn alloc<T>(&self, object: T) -> Ptr<T, Self> where Self: Sized;
+
+    /// Allocate an object with a lifetime and address that lives as long as the
+    /// allocator itself, i.e. is not dynamically garbage collected.
+    fn alloc_static<T>(&self, object: T) -> Ptr<T, Self> where Self: Sized;
+
+    /// Run a garbage collection iteration.
+    fn collect(&mut self);
 }
 
 
-// A trait that describes heap operations
-pub trait Heap<A: Allocator> {
-    fn alloc<T>(&self, object: T) -> Ptr<T, A> where Self: Sized;
-    fn alloc_immovable<T>(&self, object: T) -> Ptr<T, A> where Self: Sized;
+pub trait StackFrames {
+
 }
 
 
-pub struct Ptr<'heap, T, A: 'heap + Allocator> {
+pub trait Stack {
+    type Heap: Allocator;
+    type Frames: StackFrames;
+
+    fn mutate<M>(mutator: &mut M) where M: FnMut(&mut Self::Frame, &mut Self::Heap);
+}
+
+
+pub struct Ptr<'storage, T, A: 'storage + Allocator> {
     ptr: *mut T,
-    _marker: PhantomData<&'heap A>
+    _marker: PhantomData<&'storage A>
 }
 
 
-impl<'heap, T, A: 'heap + Allocator> Ptr<'heap, T, A> {
+impl<'storage, T, A: 'storage + Allocator> Ptr<'storage, T, A> {
     /// Pointer identity comparison
     pub fn is<'anyheap, B: 'anyheap + Allocator>(&self, other: Ptr<'anyheap, T, B>) -> bool {
         self.ptr == other.ptr
@@ -50,7 +74,7 @@ impl<'heap, T, A: 'heap + Allocator> Ptr<'heap, T, A> {
 }
 
 
-impl<'heap, T, A: 'heap + Allocator> Deref for Ptr<'heap, T, A> {
+impl<'storage, T, A: 'storage + Allocator> Deref for Ptr<'storage, T, A> {
     type Target = T;
 
     fn deref(&self) -> &T {
@@ -59,36 +83,36 @@ impl<'heap, T, A: 'heap + Allocator> Deref for Ptr<'heap, T, A> {
 }
 
 
-impl<'heap, T, A: 'heap + Allocator> DerefMut for Ptr<'heap, T, A> {
+impl<'storage, T, A: 'storage + Allocator> DerefMut for Ptr<'storage, T, A> {
     fn deref_mut(&mut self) -> &mut T {
         unsafe { &mut *self.ptr }
     }
 }
 
 
-impl<'heap, T: Hash, A: 'heap + Allocator> Hash for Ptr<'heap, T, A> {
+impl<'storage, T: Hash, A: 'storage + Allocator> Hash for Ptr<'storage, T, A> {
     fn hash<H: Hasher>(&self, state: &mut H) {
         Self::deref(self).hash(state);
     }
 }
 
 
-impl<'heap, T, A: 'heap + Allocator> PartialEq for Ptr<'heap, T, A> {
-    fn eq(&self, other: &Ptr<'heap, T, A>) -> bool {
+impl<'storage, T, A: 'storage + Allocator> PartialEq for Ptr<'storage, T, A> {
+    fn eq(&self, other: &Ptr<'storage, T, A>) -> bool {
         self.ptr == other.ptr
     }
 }
 
 
-impl<'heap, T, A: 'heap + Allocator> Eq for Ptr<'heap, T, A> {}
+impl<'storage, T, A: 'storage + Allocator> Eq for Ptr<'storage, T, A> {}
 
 
-impl<'heap, T, A: 'heap + Allocator> Copy for Ptr<'heap, T, A> {}
+impl<'storage, T, A: 'storage + Allocator> Copy for Ptr<'storage, T, A> {}
 
 
 // We don't want to force A to be Clone, so we can't #[derive(Copy, Clone)]
-impl<'heap, T, A: 'heap + Allocator> Clone for Ptr<'heap, T, A> {
-    fn clone(&self) -> Ptr<'heap, T, A> {
+impl<'storage, T, A: 'storage + Allocator> Clone for Ptr<'storage, T, A> {
+    fn clone(&self) -> Ptr<'storage, T, A> {
         Ptr {
             ptr: self.ptr,
             _marker: PhantomData
